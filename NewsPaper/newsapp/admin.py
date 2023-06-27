635d535eb39e08e07e2bfe54f2b092b0e1d75f55
Vladimir <120676027+VladimirVikB@users.noreply.github.com>
2023-06-27 16:44:43 +0300

    add d3

--- NewsPaper/newsapp/admin.py
+++ NewsPaper/newsapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Author, Category, PostCategory, Post, Comment
 
-# Register your models here.
+admin.site.register(Author)
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(PostCategory)
+admin.site.register(Comment)
--- NewsPaper/newsapp/views.py
+++ NewsPaper/newsapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+
+class PostList(ListView):
+    model = Post
+    template_name = 'posts.html'
+    context_object_name = 'posts'  # имя списка в котором лежат все объекты
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
