ab28ad1dc102de1176939c96af08396155f4ea2f
Vladimir <120676027+VladimirVikB@users.noreply.github.com>
2023-07-01 15:15:03 +0300

    add d3

--- NewsPaper/newsapp/urls.py
+++ NewsPaper/newsapp/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import PostList, PostDetail
+from .views import PostList, PostDetail, PostSearch
 
 urlpatterns = [
     path('', PostList.as_view()),  # т.к. сам по себе это класс, то нам надо представить
     # этот класс в виде view. Для этого вызываем метод as_view
     path('<int:pk>', PostDetail.as_view()),
-    path('/search', PostDetail.as_view()),
+    path('search', PostSearch.as_view()),
 ]
--- NewsPaper/newsapp/views.py
+++ NewsPaper/newsapp/views.py
@@ -11,13 +11,20 @@ class PostList(ListView):
     context_object_name = 'posts'  # имя списка в котором лежат все объекты
     paginate_by = 2  #вывод по 10 новостей
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())
-        return context
-
 
 class PostDetail(DetailView):
     model = Post
     template_name = 'post.html'
     context_object_name = 'post'  # название объекта ghgfh
+
+
+class PostSearch(ListView):
+    model = Post
+    template_name = 'post_search.html'
+    context_object_name = 'posts'  # имя списка в котором лежат все объекты
+    paginate_by = 1
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())
+        return context
