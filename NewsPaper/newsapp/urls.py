e066d7bdd9280a936e97e0976b325a8ef2210f4e
Vladimir <120676027+VladimirVikB@users.noreply.github.com>
2023-07-13 17:01:19 +0300

    add allauth

--- NewsPaper/newsapp/urls.py
+++ NewsPaper/newsapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import PostList, PostDetail, PostSearch, PostAdd, PostEdit, PostDelete
 
 urlpatterns = [
-    path('', PostList.as_view()),  # т.к. сам по себе это класс, то нам надо представить
+    path('', PostList.as_view(), name='post_list'),  # т.к. сам по себе это класс, то нам надо представить
     # этот класс в виде view. Для этого вызываем метод as_view
     path('<int:pk>', PostDetail.as_view()),
     path('search', PostSearch.as_view()),
