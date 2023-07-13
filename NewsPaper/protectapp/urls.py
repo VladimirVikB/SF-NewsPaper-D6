af213d6c4d45146d1ddaa839b6220018c68328e8
Vladimir <120676027+VladimirVikB@users.noreply.github.com>
2023-07-13 14:20:45 +0300

    add allauth

--- NewsPaper/protectapp/urls.py
+++ NewsPaper/protectapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, upgrade_me
 
-
+app_name = 'protectapp'
 urlpatterns = [
-    path('', IndexView.as_view()),
+    path('', IndexView.as_view(), name='index'),
+    path('upgrade/', upgrade_me, name='upgrade')
 ]
--- NewsPaper/protectapp/views.py
+++ NewsPaper/protectapp/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 class IndexView(LoginRequiredMixin, TemplateView):
     template_name = 'protectapp/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_not_authors'] = not self.request.user.groups.filter(name='authors').exists()
+        return context
+
+
+@login_required
+def upgrade_me(request):
+    user = request.user
+    premium_group = Group.objects.get(name='premium')
+    if not request.user.groups.filter(name='premium').exists():
+        premium_group.user_set.add(user)
+    return redirect('/')
