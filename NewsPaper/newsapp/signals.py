413e5bdc55a4a0926e06d1c9ca987994dd54992f
Vladimir <120676027+VladimirVikB@users.noreply.github.com>
2023-08-05 23:40:51 +0300

    add appointapp

--- NewsPaper/newsapp/signals.py
+++ NewsPaper/newsapp/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
 
 from .models import PostCategory
-from .tasks import new_post_subscription
+from newsapp.tasks import new_post_subscription
 
 
 @receiver(m2m_changed, sender=PostCategory)
